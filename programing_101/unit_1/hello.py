9ec835ecae3902aff90d569819200c1f5a3b73a7
Justleecho <MrEricLe@gmail.com>
2020-09-21 20:52:24 -0700

    Day 1

--- programing_101/unit_1/hello.py
+++ programing_101/unit_1/hello.py
@@ -1,2 +1,15 @@
-print('hello world!')
-print(5 + 5)
+#Lab 1
+
+#Variables
+space = " "
+
+#Program Start#
+
+# Basic String
+print("This is a basic String.")
+# Basic String with escape words
+print("This is a \"String\" using escape characters.")
+# String with Concatenating and String Methods and with a Variables
+print ("this".capitalize() + space + "is" + space + "a" + " " + "String using concatenation,string methods and a variable.")
+
+#Program End#
